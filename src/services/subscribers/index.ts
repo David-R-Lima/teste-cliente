7032abb27945e6d78b0ba523fa8dd39a776b78dc
David <david.lima@bttis.com>
2024-04-11 17:17:49 -0300

    changes

--- src/services/subscribers/index.ts
+++ src/services/subscribers/index.ts
@@ -3,7 +3,6 @@ import { Subscriber } from "./types"
 
 export const getSubscriptions = async () => {
     const { data } = await api.get<{subscribers: Subscriber[]}>('/subscriptions?merchant_id=MERCHANT_TEST_ID')
-    console.log('data: ', data);
 
     return data.subscribers
 }

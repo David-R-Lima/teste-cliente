5ef99653bbf0191e0724cf394c399e5d47feec0a
David <david.lima@bttis.com>
2024-07-17 16:08:06 -0300

    changes

--- src/services/cupons/types.ts
+++ src/services/cupons/types.ts
@@ -1,17 +1,3 @@
-// return {
-//     id: cupom.id.toString(),
-//     code: cupom.code,
-//     value: cupom.value,
-//     cupom_payment_type: cupom.cupomPaymentType,
-//     cupom_value_type: cupom.cupomValueType,
-//     expiration_date: cupom.expirationDate,
-//     max_number_of_uses: cupom.maxNumberOfUses,
-//     number_of_uses: cupom.numberOfUses,
-//     merchant_id: cupom.merchantId?.toString(),
-//     created_at: cupom.createdAt,
-//     updated_at: cupom.updatedAt,
-//   };
-
 import { ChargeType } from '../charges/types'
 
 export enum CupomValueType {

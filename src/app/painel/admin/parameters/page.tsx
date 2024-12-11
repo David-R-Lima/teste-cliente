43218c568d2935002d13a9413c60bc327c154259
pauloDiasBttis <paulo.dias@bttis.com>
2024-12-11 15:24:18 -0300

    show merchant parameters

--- src/app/painel/admin/parameters/page.tsx
+++ src/app/painel/admin/parameters/page.tsx
@@ -1,43 +1,16 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import {
   getMerchantPameters,
   MerchantParameterProps,
 } from '@/services/reports/pagbttis/get-merchant-parameters'
 
-import {
-  ImerchantParameters,
-  setMerchantSettings,
-} from '@/services/reports/pagbttis/set-merchant-parameters'
 import { useEffect, useState } from 'react'
 
 export default function MerchantParameters() {
   const [dataParameters, setDataParameters] = useState<
     MerchantParameterProps[]
   >([])
-
-  const [reload, setReload] = useState(false)
-
-  const formSubmit = async ({
-    name,
-    description,
-    group,
-    type,
-  }: ImerchantParameters) => {
-    const res = await setMerchantSettings({ name, description, group, type })
-
-    console.log('resultado form submit ---', res)
-
-    if (!res) {
-      alert('Erro ao salvar parametro')
-    } else {
-      alert('Parametro salvo')
-    }
-    setReload(!reload)
-  }
 
   useEffect(() => {
     const fetchParameters = async () => {
@@ -51,45 +24,21 @@ export default function MerchantParameters() {
     }
 
     fetchParameters()
-  }, [reload])
+  }, [])
 
   return (
     <div>
-      <span className="text-lg"> Parâmetros dos vendedores</span>
-      <div className="flex flex-rol gap-8 mb-10 justify-between   flex-wrap border-2 p-6">
-        <div className="mb-4">
-          <Button> Novo</Button>
-          <form>
-            <Label> Nome</Label>
-            <Input defaultValue={''} />
-
-            <Label> Descrição</Label>
-            <Input defaultValue={''} />
-
-            <Label> Grupo</Label>
-            <Input defaultValue={''} />
-
-            <Label> Tipo</Label>
-            <Input defaultValue={''} />
-            <Button className="mt-4"> Salvar</Button>
-          </form>
-        </div>
-
+      <span className="text-xl text-gray-500"> Parâmetros dos vendedores</span>
+      <div className="flex flex-rol gap-8 mb-10 justify-between   flex-wrap border-2 p-6 text-sm">
         {dataParameters && (
           <ul className="flex flex-col gap-4">
-            {dataParameters?.map((item, index) => (
-              <li key={index} className="border-t-2 border-b-slate-500">
-                {' '}
-                Nome parâmetro: {item.name} - Descrição: {item.description} -
-                Tipo: {item.type} - Grupo: {item.group}{' '}
-                <Button
-                  className="bg-green-300 p-1 hover:bg-green-500"
-                  size={'sm'}
-                  onClick={() => {}}
-                >
-                  {' '}
-                  Salvar
-                </Button>
+            {dataParameters?.map((item) => (
+              <li key={item.id} className="border-t-2 border-b-slate-500">
+                <p>ID: {item.id}</p>
+                <p>Nome parâmetro: {item.name}</p>
+                <p> Descrição: {item.description}</p>
+                <p> Tipo: {item.type}</p>
+                <p>Grupo: {item.group}</p>
               </li>
             ))}
           </ul>

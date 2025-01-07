d92d20abb209f301db60f578df84881d6a77fbd4
David <david.lima@bttis.com>
2025-01-07 16:49:34 -0300

    asdad

---
+++ src/app/dashboard/payment-links/plans-columns.tsx
@@ -0,0 +1,125 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { ColumnDef } from '@tanstack/react-table'
+import { ArrowUpDown, Clipboard } from 'lucide-react'
+import { Plans } from '@/services/products/plans/types'
+import { PaymentLink } from '@/services/payment-link/types'
+
+export const PaymentLinksColumns: ColumnDef<PaymentLink>[] = [
+  {
+    accessorKey: 'id',
+    header: () => {
+      return (
+        <Button variant="link" className="italic font-bold">
+          Id
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const charge = row.original
+      if (charge.id) {
+        return (
+          <div className="flex items-center justify-center">
+            <Clipboard
+              className="hover:text-primary hover:cursor-pointer"
+              onClick={() => {
+                window.navigator.clipboard.writeText(charge.id)
+              }}
+            ></Clipboard>
+          </div>
+        )
+      }
+    },
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="italic font-bold"
+        >
+          Nome
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'value',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="italic font-bold"
+        >
+          Valor
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'description',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="italic font-bold"
+        >
+          Descrição
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'period_type',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="italic font-bold"
+        >
+          Tipo do período
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: 'test_days',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="link"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          className="italic font-bold"
+        >
+          Dias de teste
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  // {
+  //   id: 'actions',
+  //   cell: ({ row }) => {
+  //     const promptGroup = row.original
+  //     return (
+  //       <Popover>
+  //         <PopoverTrigger>
+  //           <MoreVertical />
+  //         </PopoverTrigger>
+  //         <PopoverContent className="w-full">
+  //         </PopoverContent>
+  //       </Popover>
+  //     )
+  //   },
+  // },
+]
--- src/services/payment-link/index.ts
+++ src/services/payment-link/index.ts
@@ -29,3 +29,34 @@ export function fetchPaymentLink(
 
   return promise as Promise<PaymentLink>
 }
+
+export function fetchAllPaymentLink(
+  ctx: QueryFunctionContext,
+): Promise<PaymentLink[]> {
+  const [, id] = ctx.queryKey
+
+  const paymentLink: PaymentLink[] = [
+    {
+      id: id as string,
+      url: 'https://example.com/payment/12345',
+      name: 'Payment Link 1',
+      description: 'This is a test payment link',
+      endDate: new Date(),
+      value: 10000,
+      billingType: 'subscription',
+      chargeType: 'recurring',
+      dueDateLimitDays: 30,
+      subscriptionCycle: 'monthly',
+      maxInstallmentCount: 10,
+      notificationEnabled: true,
+    },
+  ]
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(paymentLink)
+    }, 2000) // Simulate API request delay
+  })
+
+  return promise as Promise<PaymentLink[]>
+}

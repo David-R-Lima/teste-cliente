6ff1a6582497e5f0d0993e9261f4d25058b7e094
David <david.lima@bttis.com>
2025-01-10 11:21:14 -0300

    changes

--- src/app/payment-link/[...id]/page.tsx
+++ src/app/payment-link/[...id]/page.tsx
@@ -110,36 +110,52 @@ export default function PaymentLink() {
     }
   }, [paymentLinkQuery.data])
 
-  const tokenize = async () => {
-    if (paymentLinkQuery.data?.publicKey) {
-      BttisCreditCard.setPubKey(paymentLinkQuery.data.publicKey).setCreditCard({
-        number: cardToTokenize.card_number,
-        cvc: cardToTokenize.card_cvv,
-        expirationMonth: cardToTokenize.card_expiration_month,
-        expirationYear: cardToTokenize.card_expiration_year,
-        cardHolder: cardToTokenize.card_holder,
-        cpf: cardToTokenize.cpf,
-      })
+  const tokenize = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        if (paymentLinkQuery.data?.publicKey) {
+          BttisCreditCard.setPubKey(
+            paymentLinkQuery.data.publicKey,
+          ).setCreditCard({
+            number: cardToTokenize.card_number,
+            cvc: cardToTokenize.card_cvv,
+            expirationMonth: cardToTokenize.card_expiration_month,
+            expirationYear: cardToTokenize.card_expiration_year,
+            cardHolder: cardToTokenize.card_holder,
+            cpf: cardToTokenize.cpf,
+          })
 
-      const card = await BttisCreditCard.hash()
+          const card = async () => {
+            await BttisCreditCard.hash()
+              .then((data) => {
+                console.log(data)
+                setValue('card_token', data.value)
+                resolve(data.value)
+              })
+              .catch((err) => {
+                reject(new Error(err.value))
+              })
+          }
 
-      if (card && card.error) {
-        toast.error(card.value)
-        return
+          card()
+        } else {
+          const errorMessage = 'Error ao tokenizar cartão'
+          toast.error(errorMessage)
+          reject(new Error(errorMessage))
+        }
+      } catch (error) {
+        toast.error('Unexpected error occurred during tokenization')
+        reject(error)
       }
-
-      if (card) {
-        setValue('card_token', card.value)
-      }
-    } else {
-      toast.error('Error ao tokenizar cartão')
-    }
+    })
   }
 
-  const handleSubmitMutation = () => {
+  const handleSubmitMutation = async () => {
     if (paymentType === PaymentType.CREDIT_CARD) {
-      tokenize()
+      await tokenize()
     }
+
+    console.log(getValues())
 
     payPaymentLinkMutation.mutate({
       ...getValues(),
@@ -238,35 +254,44 @@ export default function PaymentLink() {
           {step === 3 && paymentType === PaymentType.CREDIT_CARD && (
             <>
               <div className="mt-10 space-y-2">
-                <Input
-                  placeholder="Titular do cartão"
-                  onChange={(e) => {
-                    setCardToTokenize({
-                      ...cardToTokenize,
-                      card_holder: e.currentTarget.value,
-                    })
-                  }}
-                ></Input>
-                <Input
-                  placeholder="Cpf do dono do cartão"
-                  onChange={(e) => {
-                    setCardToTokenize({
-                      ...cardToTokenize,
-                      cpf: e.currentTarget.value,
-                    })
-                  }}
-                ></Input>
-                <Input
-                  placeholder="Número do cartão"
-                  onChange={(e) => {
-                    setCardToTokenize({
-                      ...cardToTokenize,
-                      card_number: e.currentTarget.value,
-                    })
-                  }}
-                ></Input>
-                <div className="flex space-x-2">
-                  <div className="flex items-center justify-between w-full">
+                <div>
+                  <Label>Titular</Label>
+                  <Input
+                    placeholder="Titular do cartão"
+                    onChange={(e) => {
+                      setCardToTokenize({
+                        ...cardToTokenize,
+                        card_holder: e.currentTarget.value,
+                      })
+                    }}
+                  ></Input>
+                </div>
+                <div>
+                  <Label>Cpf</Label>
+                  <Input
+                    placeholder="Cpf do dono do cartão"
+                    onChange={(e) => {
+                      setCardToTokenize({
+                        ...cardToTokenize,
+                        cpf: e.currentTarget.value,
+                      })
+                    }}
+                  ></Input>
+                </div>
+                <div>
+                  <Label>Número</Label>
+                  <Input
+                    placeholder="Número do cartão"
+                    onChange={(e) => {
+                      setCardToTokenize({
+                        ...cardToTokenize,
+                        card_number: e.currentTarget.value,
+                      })
+                    }}
+                  ></Input>
+                </div>
+                <div className="flex space-x-2 justify-between">
+                  <div className="flex justify-start items-center space-x-2">
                     <div>
                       <Label>Expiration month</Label>
                       <Input
@@ -296,15 +321,19 @@ export default function PaymentLink() {
                       ></Input>
                     </div>
                   </div>
-                  <Input
-                    placeholder="CVV"
-                    onChange={(e) => {
-                      setCardToTokenize({
-                        ...cardToTokenize,
-                        card_cvv: e.currentTarget.value,
-                      })
-                    }}
-                  ></Input>
+                  <div>
+                    {' '}
+                    <Label>Expiration month</Label>
+                    <Input
+                      placeholder="CVV"
+                      onChange={(e) => {
+                        setCardToTokenize({
+                          ...cardToTokenize,
+                          card_cvv: e.currentTarget.value,
+                        })
+                      }}
+                    ></Input>
+                  </div>
                 </div>
               </div>
               {payPaymentLinkMutation.isPending ? (

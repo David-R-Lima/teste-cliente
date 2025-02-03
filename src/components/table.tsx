9d1ed0c67715e4a86ad5fb9972e98a221b30277f
David <david.lima@bttis.com>
2025-02-03 16:41:56 -0300

    pagination

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -14,6 +14,7 @@ import {
   Table,
   TableBody,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -33,9 +34,7 @@ import {
 } from '@/components/ui/pagination'
 
 import { useState } from 'react'
-import { Button } from './ui/button'
 import { ChevronLeft, ChevronRight, CircleX, Loader2 } from 'lucide-react'
-import { Input } from './ui/input'
 
 interface DataTableProps<TData, TValue> {
   name: string
@@ -67,47 +66,78 @@ export function TableComponent<TData, TValue>({
   })
 
   return (
-    <Table className="rounded-lg overflow-hidden">
-      <TableHeader className="bg-primary-foreground">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <TableHead key={header.id} className="text-center">
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext(),
-                  )}
-                </TableHead>
-              )
-            })}
-          </TableRow>
-        ))}
-      </TableHeader>
-      <TableBody className="w-full">
-        {table.getRowModel().rows?.length ? (
-          table.getRowModel().rows.map((row) => (
-            <TableRow
-              key={row.id}
-              data-state={row.getIsSelected() && 'selected'}
-              className="flex-col justify-start items-start even:bg-gray-100"
-            >
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id} className="text-center">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
+    <div>
+      <Table className="rounded-lg overflow-hidden">
+        <TableHeader className="bg-primary-foreground">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="text-center">
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  </TableHead>
+                )
+              })}
             </TableRow>
-          ))
-        ) : (
-          <TableRow className="relative">
-            <TableCell colSpan={columns.length} className="text-center ">
-              Sem resultados.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+          ))}
+        </TableHeader>
+        <TableBody className="w-full">
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+                className="flex-col justify-start items-start even:bg-gray-100"
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id} className="text-center">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow className="relative">
+              <TableCell colSpan={columns.length} className="text-center ">
+                Sem resultados.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <div className="mt-4">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem
+              onClick={() => {
+                if(page > 1) {
+                  setPage(page - 1)
+                }
+              }}
+              className={`${page <= 1 ? 'bg-primary-foreground' :'bg-primary' } size-8 rounded-sm hover:cursor-pointer`}
+            >
+              <ChevronLeft className="size-8 text-secondary"></ChevronLeft>
+            </PaginationItem>
+            <PaginationItem className="flex items-center justify-center bg-primary size-8 rounded-sm">
+            <p className='text-secondary font-bold text-xl'>{page}</p>
+            </PaginationItem>
+            <PaginationItem
+                            onClick={() => {
+                              if(data.length === 10) {
+                                setPage(page + 1)
+                              }
+                            }}
+                            className={`${data.length !== 10 ? 'bg-primary-foreground' :'bg-primary' } size-8 rounded-sm hover:cursor-pointer`}
+            >
+              <ChevronRight className="size-8 text-secondary"></ChevronRight>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </div>
   )
 }
 
